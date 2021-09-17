6696fa57769059812ae84ed7f1025953b977f8eb
Jonas Stubenrauch <jonas.stubenrauch@arconsis.com>
2021-09-17 10:12:18 +0200

    add unit tests

--- Targets/MovieTime/Tests/AppTests.swift
+++ Targets/MovieTime/Tests/AppTests.swift
@@ -1,8 +1,75 @@
 import Foundation
 import XCTest
+import ComposableArchitecture
+import Combine
+
+@testable import MovieTime
+import MovieApi
 
 final class MovieTimeTests: XCTestCase {
-    func test_twoPlusTwo_isFour() {
-        XCTAssertEqual(2+2, 4)
+    
+    let scheduler = DispatchQueue.test
+
+    func createEnvironment(movies: [Movie] = []) -> MovieListEnvironment {
+        MovieListEnvironment(
+            mainQueue: scheduler.eraseToAnyScheduler(),
+            search: { _ in Effect(value: movies).eraseToAnyPublisher() },
+            load: { _ in Fail(error: MovieApi.Error.detail).eraseToAnyPublisher() }
+        )
+    }
+    
+    func createEnvironment(movies: @escaping (String) -> [Movie]) -> MovieListEnvironment {
+        MovieListEnvironment(
+            mainQueue: scheduler.eraseToAnyScheduler(),
+            search: { query in Effect(value: movies(query)).eraseToAnyPublisher() },
+            load: { _ in Fail(error: MovieApi.Error.detail).eraseToAnyPublisher() }
+        )
+    }
+    
+    func test_noSearchResults() {
+        
+        let store = TestStore(
+            initialState: MovieListState(),
+            reducer: movieListReducer,
+            environment: createEnvironment()
+        )
+        
+        store.send(MovieListAction.searchFieldChanged("test")) {
+            $0.searchTerm = "test"
+        }
+        
+        scheduler.advance(by: 0.5)
+            
+        store.receive(MovieListAction.search("test"))
+        
+        store.receive(MovieListAction.showMovies(.success([]))) {
+            $0.movieStates = []
+        }
+    }
+    
+    func test_noSearchTerm() {
+        
+        let store = TestStore(
+            initialState: MovieListState(),
+            reducer: movieListReducer,
+            environment: createEnvironment(
+                movies: { _ in
+                    XCTFail("Api should not be called here")
+                    return []
+                }
+            )
+        )
+        
+        store.send(MovieListAction.searchFieldChanged("")) {
+            $0.searchTerm = ""
+        }
+        
+        scheduler.advance(by: 0.5)
+            
+        store.receive(MovieListAction.search(""))
+        
+        store.receive(MovieListAction.showMovies(.success([]))) {
+            $0.movieStates = []
+        }
     }
 }
